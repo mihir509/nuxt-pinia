37b0c61c1267a0ca39a015ee7fe3b07a87a1ff34
mihir.gohil <mihir.itaims@gmail.com>
2023-10-11 17:25:35 +0530

    pinia implement

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <p>Counter: {{ counterStore.count }}</p>
+        <button @click="add">increment</button>
+        <button @click="minus">dicrement</button>
+
+    </div>
+</template>
+
+<script>
+import { useFriendStore } from '@/stores/counter.js'
+
+export default defineComponent({
+    setup() {
+        const counterStore = useFriendStore()
+        return { counterStore }
+    },
+    methods:{
+        add(){
+            this.counterStore.increment()
+        },
+        minus(){
+            this.counterStore.dicrement()
+        }
+    }
+})
+</script>
